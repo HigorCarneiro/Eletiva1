e4e246f6fab45957d143777d999bbe8de18931bd
HigorCarneiro <149540741+HigorCarneiro@users.noreply.github.com>
2024-08-27 20:33:22 -0300

    .

---
+++ Aula2/exemplo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desvios Condicionais</title>
+</head>
+<body>
+    <main class ="caontainer">
+        <form action="exemplo_resposta.php" method="POST">
+            <input type="number" name="valor">
+            <button type="submit">OK</button>
+        </form>
+    </main>
+</body>
+</html>
