7e2639613e7c36d66b2a0b458ae7493f94ed20fe
HigorCarneiro <149540741+HigorCarneiro@users.noreply.github.com>
2024-08-27 19:36:47 -0300

    atualiz

--- Lista2/exer4.php
+++ Lista2/exer4.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="container">
-    <h1>Exercício 1</h1>
+    <h1>Exercício 4</h1>
     <form action="exer1resp.php" method="POST">
         <div class="row">
             <div class="col">
