c0bb06fef41bf087e1807a95a91cc2a1c158a18e
HigorCarneiro <higorcarneiro2004@gmail.com>
2024-09-03 21:03:26 -0300

    alterações

---
+++ Aula3/form2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <form action="resposta.php" method="POST">
+        <div class="col">
+            <input type="number" name="valor1" class="form-control">
+        </div>
+        <div class="col">
+            <input type="number" name="valor2" class="form-control">
+            <button type="submit">Enviar</button>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Aula3/resposta2.php
@@ -0,0 +1,39 @@
+<?php
+    declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ </head>
+<body>
+    <?php
+        function soma(int $a, int $b): int{
+            return $a + $b;
+        }
+
+        function subtracao(int $a, int $b): void{
+            echo "<p>Subtração: ".($a- $b)."</p>";
+        }
+
+        function saudacao(string $nome = "Visitante"): void{
+            echo "<p>Olá $nome!</p>";
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST"){
+            try{
+                $valor1 = (int) $_POST['valor1'] ?? 0;
+                $valor2 = (int) $_POST['valor2'] ?? 0;
+                echo"<p>Soma";
+                saudacao();
+                saudacao("Higor");
+            } catch (Exception $e){
+                echo "Erro:".$e->getMessage();
+            }
+        }
+    ?>   
+</body>
+</html>
