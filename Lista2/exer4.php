edb8ac7d5f3682e1cec5b53f49b6aa22c2d177d8
HigorCarneiro <higorcarneiro2004@gmail.com>
2024-09-21 21:00:01 -0300

    .

--- Lista2/exer4.php
+++ Lista2/exer4.php
@@ -3,12 +3,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício 1</title>
+    <title>Exercício 4</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body class="container">
     <h1>Exercício 4</h1>
-    <form action="exer1resp.php" method="POST">
+    <form action="exer4resp.php" method="POST">
         <div class="row">
             <div class="col">
                 <label for="valor1" class="form-label">Informe o valor 1 </label>
